b4088131092ad3dc1608bf875dcb3833a6c14578
Daniil <d-a-ny4@yandex.ru>
2019-05-01 17:09:54 +0500

    add environment classes

--- Keep Silence/Monster.cs
+++ Keep Silence/Monster.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Keep_Silence
 {
     public class Monster : ICreature
     {
+        public Point Position;
         public CreatureCommand MakeStep(Game game)
         {
             throw new NotImplementedException();
